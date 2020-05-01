880efbc1b7f57242907d42acd782362accb94555
adrianaaguilera <47437097+adrianaaguilera@users.noreply.github.com>
2020-05-01 06:34:19 -0700

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context)  {
+    return MaterialApp();
+    home: homeScreen;
+  }
+}
 
-class SecondScreen extends StatelessWidget {
+class homeScreen extends StatefulWidget {
+  @override
+  _CalenderState createState() => _CalenderState();
+}
+
+class _CalenderState extends State<homeScreen> {
+  CalendarController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CalendarController();
+
+    Widget build(BuildContext context) {
+      return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.grey,
+            title: Text(
+                "Day Maker",
+                style: TextStyle(
+                  color: Colors.white,
+                )
+            ),
+          ),
+          body: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TableCalendar(
+                    initialCalendarFormat: CalendarFormat.week,
+                    calendarStyle: CalendarStyle(
+                      todayColor: Colors.amber,
+                      selectedColor: Colors.grey,
+                    ),
+                    calendarController: _controller,)
+                ],
+              )
+          )
+      );
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-    );
+    // TODO: implement build
+    return null;
   }
 }
