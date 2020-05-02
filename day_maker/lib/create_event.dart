cc2d57ab94f6f967e52fe0b84200df5e29ab4015
adrianaaguilera <47437097+adrianaaguilera@users.noreply.github.com>
2020-05-01 17:55:44 -0700

    Old Code

---
+++ day_maker/lib/create_event.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class createEvent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Container(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              new Container(
+                child: Text(
+                  "Add Categroy:",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 40,
+                  ),
+                ),
+              ),
+              new Container(
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      labelText: "Title"
+                  ),
+                ),
+              ),
+              new Container(
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      labelText: "Start"
+                  ),
+                ),
+              ),
+              new Container(
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      labelText: "End"
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
