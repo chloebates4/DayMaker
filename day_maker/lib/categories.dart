52ff97dcf3f3c88e1ab979968ee34bfa9cd94841
chloebates4 <chloe.bates4@gmail.com>
2020-05-01 22:19:23 -0700

    category class

--- day_maker/lib/categories.dart
+++ day_maker/lib/categories.dart
@@ -1,4 +1,49 @@
 /*
 Calendar categories - for classes, assignments, etc.
  */
+import 'package:flutter/material.dart';
+
+class Categories extends StatelessWidget {
+
+  void view(int colorNumIdentifier) {
+    // TODO: view category
+  }
+
+  Expanded buildKey({Color inColor, int inSoundNum}) {
+    return Expanded(
+      child: FlatButton(
+        color: inColor,
+        onPressed: () {
+          view(0); //todo: change number
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                buildKey(inColor: Colors.red),
+                buildKey(inColor: Colors.orange),
+                buildKey(inColor: Colors.yellow),
+                buildKey(inColor: Colors.green),
+                buildKey(inColor: Colors.teal),
+                buildKey(inColor: Colors.blue),
+                buildKey(inColor: Colors.purple)
+              ],
+            )
+        ),
+      ),
+    );
+  }
+}
+
+
+
 
